ecaf7e513bb903d4879bc86e6ea9aeb4d9a006f9
Vikas Kumar <113631243+kalakondavikaskumar@users.noreply.github.com>
2025-04-04 13:06:17 +0530

    Add files via upload

---
+++ Library_Management/library_dbs.sql
@@ -0,0 +1,11 @@
+CREATE TABLE books (
+    bookID VARCHAR(10) PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR(255),
+    publisher VARCHAR(255),
+    year INT,
+    isbn VARCHAR(20),
+    copies INT
+);
+
+
